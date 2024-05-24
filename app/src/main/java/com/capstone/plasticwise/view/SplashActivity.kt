a84dafdcb20d8ae63cce9cde0d0e8bfedfa82fe4
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-05-24 09:15:02 +0700

    [update] at main activity ui, newest logo, login

---
+++ app/src/main/java/com/capstone/plasticwise/view/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.capstone.plasticwise
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.plasticwise.databinding.ActivitySplashBinding
+import com.capstone.plasticwise.view.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+
+
+        binding.iconPicture.startAnimation(fadeInAnimation)
+        binding.splashText.startAnimation(slideUpAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // 3 seconds delay
+    }
+}
